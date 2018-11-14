8f34fc8b7428037a441fa5407281ccca37c71122
FHandrick <francisco@francisco-VirtualBox>
2018-11-14 18:39:34 -0200

    update File generator.

---
+++ src/main/java/Comparison.java
@@ -0,0 +1,10 @@
+
+public class Comparison {
+	String commitBegin = "";
+	String commitFinal = "";
+	public Comparison(String commitBegin, String coomitFinal) {
+		super();
+		this.commitBegin = commitBegin;
+		this.commitFinal = coomitFinal;
+	}
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,65 +1,59 @@
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import apiTimeLIfe.APIDiff;
 import apiTimeLIfe.Change;
 import apiTimeLIfe.Result;
 import apiTimeLIfe.enums.Classifier;
+import apiTimeLIfe.util.UtilFile;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
 		
-		
-		FileWriter file;
-
+		String file = "";
+		char ch='"';
 		APIDiff diff = new APIDiff("bcgit/bc-java", "https://github.com/bcgit/bc-java.git");
     	diff.setPath("/home/francisco/github");
     	
+    
     	Release re = new Release();
-    	re.insert();
-    	
+    	re.distribute();
+    	ArrayList<String> prologLine = new ArrayList<String>();
     	for (int i = 0; i<re.comparison.size();i++) {
-    		System.out.println(re.comparison.get(i).commitBegin+ " Compared with: "+re.comparison.get(i).commitFinal);
-    		System.out.println( "================"+"Comparation Number:"+i+"===========================================" );
-    		
-    		
-        	file = new FileWriter(re.comparison.get(i).commitBegin+"_"+re.comparison.get(i).commitFinal+".pl");
-        	PrintWriter recordFile = new PrintWriter(file);
+    		prologLine.clear();
+    		//System.out.println(re.comparison.get(i).commitBegin+ " Compared with: "+re.comparison.get(i).commitFinal);
+    		//System.out.println( "================"+"Comparation Number:"+i+"===========================================" );
+    		file = (re.comparison.get(i).commitBegin+"_"+re.comparison.get(i).commitFinal+".pl");
         	Result result = diff.detectChangeBetweenCommits(re.comparison.get(i).commitBegin, re.comparison.get(i).commitFinal,Classifier.API);
-        	
-        	recordFile.printf("/* facts */"+"\n");
+
+        	prologLine.add("/* facts */"+"\n");
         	
         	for(Change changeMethod : result.getChangeMethod()){
-        	    
-        	    
+        		
         	    if (changeMethod.getCategory().name().toString() == "METHOD_MOVE") {
-        	    	
-        	    	
+        	    		    	
         	      	String aux = (changeMethod.getDescription()).substring(12, (changeMethod.getDescription()). lastIndexOf("</code><br>moved from"));
-        	    	aux = aux.substring(0,aux.lastIndexOf("("));
-        	    	recordFile.printf("\n"+"method(1,"+aux.toLowerCase()+","+
+        	    	
+        	      	prologLine.add("method(1,"+ch+aux+ch+","+ch+
         	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>moved from")+28,(changeMethod.getDescription()).lastIndexOf("</code><br>to")).replace(".","-").toLowerCase()+","+
-        	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>to")+20, (changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
-        	    	
-        	    	
+        	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>to")+20, (changeMethod.getDescription()).length()-11)+ch+").");
+        	    	  	
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_RENAME") {
         	    	
         	    	String aux = (changeMethod.getDescription()).substring(13, (changeMethod.getDescription()). lastIndexOf("</code><br>renamed to"));
-        	    	aux = aux.substring(0,aux.lastIndexOf("("));
         	    	
-        	    	recordFile.printf("\n"+"method(2,"+aux.toLowerCase()+","+
-        	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>renamed to")+28,(changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+","+
-        	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in")+20, (changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
+        	    	prologLine.add("method(2,"+ch+aux+ch+","+ch+
+        	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>renamed to")+28,(changeMethod.getDescription()).lastIndexOf("</code><br>in <code>"))+ch+","+ch+
+        	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in")+20, (changeMethod.getDescription()).length()-11)+ch+").");
         	    	
         	    }
         	    
         	    if (changeMethod.getCategory().name().toString() == "METHOD_REMOVE") {
         	    	
-        	    	recordFile.printf("\n"+"method(3,"+(changeMethod.getDescription()).substring(17, (changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+
-        					","+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>removed")+30, 
-        							(changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
+        	    	prologLine.add("method(3,"+ch+(changeMethod.getDescription()).substring(17, (changeMethod.getDescription()).lastIndexOf("</code><br>removed"))+ch+
+        					","+ch+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>removed")+30, 
+        							(changeMethod.getDescription()).length()-11)+ch+").");
         	    	
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_PUSH_DOWN") {
@@ -69,11 +63,10 @@ public class Test {
         	    if (changeMethod.getCategory().name().toString() == "METHOD_INLINE") {
         	    	
         	    	String aux = (changeMethod.getDescription()).substring(17, (changeMethod.getDescription()). lastIndexOf("</code><br>from"));
-        	    	aux = aux.substring(0,aux.lastIndexOf("("));
-        	    
-        	    	recordFile.printf("\n"+"method(5,"+aux.toLowerCase()+","+
+        	  
+        	    	prologLine.add("method(5,"+ch+aux+ch+","+ch+
         	    			(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>from")+22,
-        	    					(changeMethod.getDescription()).lastIndexOf("</code><br>inlined to")).replace(".","-").toLowerCase()+").");
+        	    					(changeMethod.getDescription()).lastIndexOf("</code><br>inlined to"))+ch+").");
         	    			
         	    	
         	    }
@@ -83,29 +76,29 @@ public class Test {
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_CHANGE_EXCEPTION_LIST") {
         	    	
-        	    	recordFile.printf("\n"+"method(7,"+(changeMethod.getDescription()).substring(10, (changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+
-        					","+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in ")+20, 
-        							(changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
+        	    	prologLine.add("method(7,"+ch+(changeMethod.getDescription()).substring(10, (changeMethod.getDescription()).lastIndexOf(")</code><br>")+1)+ch+
+        					","+ch+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in ")+20, 
+        							(changeMethod.getDescription()).length()-11)+ch+").");
         	    	
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_CHANGE_RETURN_TYPE") {
         	    	
-        	    	recordFile.printf("\n"+"method(8,"+(changeMethod.getDescription()).substring(17, (changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+
-        					","+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>changed the return type")+47, 
-        							(changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
+        	    	prologLine.add("method(8,"+ch+(changeMethod.getDescription()).substring(17, (changeMethod.getDescription()).lastIndexOf("</code><br>changed the return type"))+ch+
+        					","+ch+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>changed the return type")+47, 
+        							(changeMethod.getDescription()).length()-11)+ch+").");
         	    	
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_LOST_VISIBILITY") {
         	    
-        	    	recordFile.printf("\n"+"method(9,"+(changeMethod.getDescription()).substring(18, (changeMethod.getDescription()). lastIndexOf("(")).toLowerCase()+
-        					","+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in <code>")+20, 
-        							(changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
+        	    	prologLine.add("method(9,"+ch+(changeMethod.getDescription()).substring(18, (changeMethod.getDescription()). lastIndexOf("</code><br> changed visibility"))+ch+
+        					","+ch+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in <code>")+20, 
+        							(changeMethod.getDescription()).length()-11)+ch+").");
         	    	
         	    	
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_ADD_MODIFIER_FINAL") {
         	    	
-        	    	recordFile.printf("\n"+"method(10,"+(changeMethod.getDescription()).substring(13, (changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+
+        	    	prologLine.add("method(10,"+(changeMethod.getDescription()).substring(13, (changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+
         					","+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("<code>final</code><br>in")+31, 
         							(changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
         	    	
@@ -113,13 +106,14 @@ public class Test {
         	    }
         	    if (changeMethod.getCategory().name().toString() == "METHOD_REMOVE_MODIFIER_STATIC") {
         	    
-        	    	recordFile.printf("\n"+"method(11,"+(changeMethod.getDescription()).substring(13, (changeMethod.getDescription()).lastIndexOf("(")).toLowerCase()+
-        					","+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in class ")+26, 
-        							(changeMethod.getDescription()).length()-11).replace(".","-").toLowerCase()+").");
+        	    	prologLine.add("method(11,"+ch+(changeMethod.getDescription()).substring(13, (changeMethod.getDescription()).lastIndexOf(")</code><br>")+1)+ch+
+        					","+ch+(changeMethod.getDescription()).substring((changeMethod.getDescription()).lastIndexOf("</code><br>in class ")+26, 
+        							(changeMethod.getDescription()).length()-11)+ch+").");
         	    	
         	    }	    
         	    
         	}
+        	
         	/*
         	for(Change changeType : result.getChangeType()){
         		
@@ -153,23 +147,22 @@ public class Test {
         	}
         	*/
     		
-        	recordFile.printf("\n");
-        	recordFile.printf("\n"+"method_move_count(Y):-findall(X,method(1,A,B,C),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_rename_count(Y):-findall(X,method(2,A,B,C),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_remove_count(Y):-findall(X,method(3,A,B),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_inline_count(Y):-findall(X,method(5,A,B),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_change_exception_list_count(Y):-findall(X,method(7,A,B),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_change_return_type_count(Y):-findall(X,method(8,A,B),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_lost_visibility_count(Y):-findall(X,method(9,A,B),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_add_modifier_final_count(Y):-findall(X,method(10,A,B),L),length(L,Y).");
-        	recordFile.printf("\n"+"method_remove_modifier_static_count(Y):-findall(X,method(11,A,B),L),length(L,Y).");
-        	recordFile.printf("\n");
-        	recordFile.printf("\n"+"all_method_change(A,B,C,D,E,F,G,H,I):-method_move_count(A),method_rename_count(B),method_remove_count(C),");
-        	recordFile.printf("\n"+"method_inline_count(D),method_change_exception_list_count(E),method_change_return_type_count(F),method_lost_visibility_count(G),");
-        	recordFile.printf("\n"+"method_add_modifier_final_count(H),method_remove_modifier_static_count(I).");
-
-        
-        	recordFile.close();
+        	prologLine.add("\n");
+        	prologLine.add("method_move_count(Y):-findall(X,method(1,A,B,C),L),length(L,Y).");
+        	prologLine.add("method_rename_count(Y):-findall(X,method(2,A,B,C),L),length(L,Y).");
+        	prologLine.add("method_remove_count(Y):-findall(X,method(3,A,B),L),length(L,Y).");
+        	prologLine.add("method_inline_count(Y):-findall(X,method(5,A,B),L),length(L,Y).");
+        	prologLine.add("method_change_exception_list_count(Y):-findall(X,method(7,A,B),L),length(L,Y).");
+        	prologLine.add("method_change_return_type_count(Y):-findall(X,method(8,A,B),L),length(L,Y).");
+        	prologLine.add("method_lost_visibility_count(Y):-findall(X,method(9,A,B),L),length(L,Y).");
+        	prologLine.add("method_add_modifier_final_count(Y):-findall(X,method(10,A,B),L),length(L,Y).");
+        	prologLine.add("method_remove_modifier_static_count(Y):-findall(X,method(11,A,B),L),length(L,Y).");
+        	prologLine.add("\n");
+        	prologLine.add("all_method_change(A,B,C,D,E,F,G,H,I):-method_move_count(A),method_rename_count(B),method_remove_count(C),");
+        	prologLine.add("method_inline_count(D),method_change_exception_list_count(E),method_change_return_type_count(F),method_lost_visibility_count(G),");
+        	prologLine.add("method_add_modifier_final_count(H),method_remove_modifier_static_count(I).");
+        	
+        	UtilFile.writeFile(file, prologLine);
         	
     	}
     		
